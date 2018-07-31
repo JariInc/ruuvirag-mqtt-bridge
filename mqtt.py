c5d38c9111c8ba24b71a68a568bd03ce17bfbda1
Jari Ylimäinen <jari@ylimainen.fi>
2018-07-31 09:01:18 +0000

    Add keepalive

--- mqtt.py
+++ mqtt.py
@@ -1,12 +1,14 @@
 import paho.mqtt.client as mqtt
 
 class MQTTClient(object):
+	keepalive = 60
+
 	def  __init__(self, client_id):
 		self.client_id = client_id
 
 	def connect(self, address, port):
 		self.client = mqtt.Client(self.client_id)
-		self.client.connect(address, port)
+		self.client.connect(address, port, self.keepalive)
 
 	def publish(self, topic, payload):
 		self.client.publish(topic, payload)
